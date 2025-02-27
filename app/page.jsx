0a292371ee23be2f0e74147526e23796bd7a5298
Shob3r <andrewpinter23@gmail.com>
2025-02-27 14:02:56 -0800

    slowly learning how tailwind and jsx works

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-8xl text-center">Coming Soon...</p>
-        <p className="text-3xl text-center">Website Under Reconstruction</p>
+        <div className="grid justify-items-center">
+          <p className="text-9xl mb-5">Coming Soon....</p>
+          <p className="text-2xl">Website under reconstruction</p>
+        </div>
       </main>
     </div>
   );
